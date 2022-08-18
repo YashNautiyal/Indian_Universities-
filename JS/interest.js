1f019c57e2fd4fc41c52b8d5167f7bf26928faec
YashNautiyal <75678778+YashNautiyal@users.noreply.github.com>
2022-08-18 16:31:42 +0530

    Add files via upload

---
+++ JS/interest.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+     $("h3#one").click(function(q){
+         q.preventDefault();
+         $("#course1").toggle();
+     });
+ });
+ $(document).ready(function(){
+    $("h3#two").click(function(q){
+        q.preventDefault();
+        $("#course2").toggle();
+    });
+});
+$(document).ready(function(){
+    $("h3#three").click(function(q){
+        q.preventDefault();
+        $("#course3").toggle();
+    });
+});
+$(document).ready(function(){
+    $("h3#four").click(function(q){
+        q.preventDefault();
+        $("#course4").toggle();
+    });
+});
+$(document).ready(function(){
+    $("h3#five").click(function(q){
+        q.preventDefault();
+        $("#course5").toggle();
+    });
+});
+$(document).ready(function(){
+    $("h3#six").click(function(q){
+        q.preventDefault();
+        $("#course6").toggle();
+    });
+});
+
+
+
